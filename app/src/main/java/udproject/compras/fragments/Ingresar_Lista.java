3d80ea323f8c0b42263c12b9246f5c75a0011b77
Amanda <luce0967@gmail.com>
2020-12-05 10:30:01 -0500

    Initial commit

---
+++ app/src/main/java/udproject/compras/fragments/Ingresar_Lista.java
@@ -0,0 +1,61 @@
+package udproject.compras.fragments;
+
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import udproject.compras.Adapters.Item_Producto;
+import udproject.compras.CameraScanner;
+import udproject.compras.FrDialog.FragmentDialogPresupuesto;
+import udproject.compras.FrDialog.IngresarPorTexto;
+import udproject.compras.R;
+import udproject.compras.firebase.LocalDB;
+import udproject.compras.recycler.RecyclerProductAdapter;
+
+public class Ingresar_Lista extends Fragment implements RecyclerProductAdapter.OnProductListener {
+
+    private List<Item_Producto>itemProducto=new ArrayList<>();
+    private RecyclerView RecyclerItemProductos;
+    private RecyclerProductAdapter AdaptadorProducto;
+    View view;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        view=inflater.inflate(R.layout.fragment_ingresar__lista, container, false);
+
+        RecyclerItemProductos=view.findViewById(R.id.RecyclerProductos);
+        RecyclerItemProductos.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        LocalDB local=new LocalDB(getContext());
+        AdaptadorProducto = new RecyclerProductAdapter(local.ListaProducto(),this);
+        RecyclerItemProductos.setAdapter(AdaptadorProducto);
+
+        return view;
+
+
+        /*int ID, String Nombre, int Precio, String Descripcion*/
+    }
+
+
+    @Override
+    public void onProductClick(int posicion) {
+
+    }
+}
---
+++ app/src/main/java/udproject/compras/recycler/RecyclerProductAdapter.java
@@ -0,0 +1,80 @@
+package udproject.compras.recycler;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.facebook.shimmer.ShimmerFrameLayout;
+
+import java.util.List;
+
+import udproject.compras.Adapters.Item_Producto;
+import udproject.compras.R;
+
+public class RecyclerProductAdapter extends RecyclerView.Adapter<RecyclerProductAdapter.ViewHolder> {
+
+    OnProductListener mProductListener;
+    public boolean showShimmer;
+    int SHIMMER_ITEMS=7;
+
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener
+    {
+        private TextView Nombre, Precio, Descripcion;
+        private Button Sumar, Restar;
+
+        OnProductListener productListener;
+
+        ShimmerFrameLayout shimmerFrame;
+        public ViewHolder(View itemView, OnProductListener productListener)
+        {
+            super(itemView);
+            shimmerFrame=itemView.findViewById(R.id.ShimmerItemProducto);
+            Nombre=itemView.findViewById(R.id.TituloProducto);
+            Precio=itemView.findViewById(R.id.PrecioProducto);
+            Descripcion=itemView.findViewById(R.id.CantidadProducto);
+        }
+        @Override
+        public void onClick(View view) {
+            productListener.onProductClick(getAdapterPosition());
+        }
+    }
+
+    public List<Item_Producto> productoList;
+
+    public  RecyclerProductAdapter(List<Item_Producto>productoList, OnProductListener productListener)
+    {
+        this.productoList=productoList;
+        this.mProductListener=productListener;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.item_producto, parent, false);
+        ViewHolder viewHolder=new ViewHolder(view, mProductListener);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+    holder.Nombre.setText(productoList.get(position).getNombre());
+    holder.Descripcion.setText(productoList.get(position).getDescripcion());
+    holder.Precio.setText(Integer.toString(productoList.get(position).getPrecio()));
+    }
+
+    public interface OnProductListener
+    {
+        void onProductClick(int posicion);
+    }
+
+    @Override
+    public int getItemCount() {
+
+        return showShimmer ? SHIMMER_ITEMS:productoList.size(); //Primero carga los items (SHIMMER) y luego la lista
+    }
+}
